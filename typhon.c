39afc06bb1dd3417d602bbe4901d031667a6b4c1
Joe Graham <jmg3484@rit.edu>
2018-02-19 22:25:45 -0500

    Pass all info into telnet

--- typhon.c
+++ typhon.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include "arghandler.h"
+#include "telnet.h"
 
 int main(int argc, char **argv) {
      struct args arg_package;
      arg_package = arg_handler(argc, argv);
-     if (arg_package.valid_args == 1) {
-          printf("Username: %s\n", arg_package.username);
-          printf("Password: %s\n", arg_package.password);
+     if (arg_package.valid_args == 0) {
+          return 1;
+     }
+     char *username = arg_package.username;
+     char *password = arg_package.password;
+     if (strncmp(arg_package.service, "telnet", 3) == 0) {
+          telnet_process(username, password, arg_package.single_user, \
+               arg_package.single_password, arg_package.hostname);
      }
      return 0;
 }
